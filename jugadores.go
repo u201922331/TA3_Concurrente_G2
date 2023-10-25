34ffc1134ff6e925c00d6cf200fe6690dd27abff
Daniel Barrionuevo <116469158+danieleub02@users.noreply.github.com>
2023-10-25 16:12:04 -0500

    Update jugadores.go

--- jugadores.go
+++ jugadores.go
@@ -1,48 +1,105 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "time"
+	"fmt"
+	"math"
+	"math/rand"
 )
 
 type Jugador struct {
-    Nombre  string
-    Fichas  []int
+	Nombre             string
+	Fichas             int
+	MovimientosTotales int
 }
 
-func (j *Jugador) LanzarDado() int {
-    rand.Seed(time.Now().UnixNano())
-    return rand.Intn(6) + 1
+func (j *Jugador) Dado() int {
+	d1 := rand.Intn(6) + 1                        // Dado 1
+	d2 := rand.Intn(6) + 1                        // Dado 2
+	s := int(math.Pow(-1, float64(rand.Intn(2)))) // Dado Signo
+	fmt.Printf("Operacion: %d + (%d * %d)", d1, s, d2)
+	return d1 + s*d2
 }
 
-func jugadorHaGanado(jugador *Jugador) bool {
-	
-    //return true
+func GenTablero(casillas uint) []rune {
+	tablero := make([]rune, casillas)
+	for i := range tablero {
+		tablero[i] = '.' // . -> Casillas en blanco
+	}
+	tablero[0] = '#'              // # -> Inicio
+	tablero[len(tablero)-1] = '$' // $ -> Fin
+
+	umbral := int(float64(casillas) * 0.4) // Solo se llenará el 40% de las casillas en blanco con casillas especiales
+
+	i := 0
+	for i < umbral {
+		idx := rand.Intn(int(casillas))
+		// No afectar las casillas inicial y final
+		if idx == 0 || idx == len(tablero)-1 {
+			continue
+		}
+
+		// 1 -> +3 espacios
+		// 2 -> -3 espacios
+		// 3 -> REGRESA AL PRINCIPIO
+		switch rand.Intn(3) + 1 {
+		case 1:
+			tablero[idx] = '1'
+		case 2:
+			tablero[idx] = '2'
+		case 3:
+			tablero[idx] = '3'
+		}
+		i++
+	}
+
+	return tablero
+}
+
+func Meta(j *Jugador) bool {
+	if j.MovimientosTotales >= 24 {
+		return true
+	}
+	return false
 }
 
 func main() {
-    jugadores := []Jugador{
-        {"Jugador 1", []int{0, 0, 0, 0}},
-        {"Jugador 2", []int{0, 0, 0, 0}},
-        {"Jugador 3", []int{0, 0, 0, 0}},
-        {"Jugador 4", []int{0, 0, 0, 0}},
-    }
+	jugadores := []Jugador{
+		{"J1", 0, 0},
+		{"J2", 0, 0},
+		{"J3", 0, 0},
+		{"J4", 0, 0},
+	}
 
-    jugadorActual := 0
+	tablero := GenTablero(60)
+	fmt.Printf("%c\n\n", tablero)
 
-    for {
-        jugador := &jugadores[jugadorActual]
+	jugadorActual := 0
 
-        dado := jugador.LanzarDado()
-        fmt.Printf("%s lanzó un %d\n", jugador.Nombre, dado)
-        if jugadorHaGanado(jugador) {
-            fmt.Printf("¡El jugador %s ha ganado!\n", jugador.Nombre)
-            break
-        }
+	for {
+		if jugadorActual == 0 {
+			fmt.Printf("----------------------------------------------\n")
+		}
 
-        jugadorActual = (jugadorActual + 1) % len(jugadores)
-    }
+		j := &jugadores[jugadorActual]
+
+		dado := j.Dado()
+		sum := j.MovimientosTotales + dado
+		j.MovimientosTotales = sum
+		fmt.Printf("\t\t%s OBTUVO: %d, SUM: %d", j.Nombre, dado, j.MovimientosTotales)
+
+		if Meta(j) {
+			j.Fichas++
+			if j.Fichas == 4 {
+				fmt.Printf("\t\t¡%s GANO!, cantidad de fichas metidas: %d\n", j.Nombre, j.Fichas)
+				break
+			}
+
+			fmt.Printf("\t\t¡%s metio una ficha!, cantidad de fichas metidas: %d", j.Nombre, j.Fichas)
+			j.MovimientosTotales = 0
+		}
+
+		jugadorActual = (jugadorActual + 1) % len(jugadores)
+
+		fmt.Printf("\n")
+	}
 }
-
-
