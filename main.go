bcbb77c3166aee140ec43bc2e259d446816656bb
u201922331 <u201922331@upc.edu.pe>
2023-10-24 21:05:48 -0500

    Dado

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func Dice() int {
+	a := rand.Intn(6) + 1                         // Dado 1
+	b := rand.Intn(6) + 1                         // Dado 2
+	s := int(math.Pow(-1, float64(rand.Intn(2)))) // Dado Signo
+	fmt.Printf("Operacion: %d + %d * %d = ", a, s, b)
+	return a + s*b
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	for i := 0; i < 15; i++ {
+		fmt.Println(Dice())
+	}
 }
